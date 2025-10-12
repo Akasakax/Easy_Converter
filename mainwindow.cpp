38310bc3ceef41225b21ccde2df3b62ae6f2a4fa
Akasaka <72452449+Akasakax@users.noreply.github.com>
2025-10-12 19:44:24 +0900

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -17,27 +17,28 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+//動画/音声ファイルを読み込む
 void MainWindow::on_pushButton_clicked()
 {
     file = QFileDialog::getOpenFileName();
     ui->label_6->setText(file);
 }
 
-
+//動画/音声を出力するフォルダを選択
 void MainWindow::on_pushButton_5_clicked()
 {
     dir = QFileDialog::getExistingDirectory();
     ui->label_7->setText(dir);
 }
 
-
+//画像ファイルを読み込む
 void MainWindow::on_pushButton_9_clicked()
 {
     file = QFileDialog::getOpenFileName();
     ui->label_5->setText(file);
 }
 
-
+//画像を出力するフォルダを選択
 void MainWindow::on_pushButton_10_clicked()
 {
     dir = QFileDialog::getExistingDirectory();
@@ -47,9 +48,11 @@ void MainWindow::on_pushButton_10_clicked()
 
 void MainWindow::on_pushButton_2_clicked()
 {
+    //ファイル名を読み込む
     QString text = ui->textEdit->toPlainText();
     QString command = "ffmpeg";
     QStringList arguments;
+    //コマンドを作成
     arguments << "-i" << file << dir + "/" + text;
     QProcess::execute(command, arguments);
     ui->label_10->setText("変換終了＆ただいま準備中…");
@@ -59,11 +62,12 @@ void MainWindow::on_pushButton_2_clicked()
 
 void MainWindow::on_pushButton_11_clicked()
 {
+    //ファイル名を読み込む
     QString text = ui->textEdit_4->toPlainText();
     QString command = "convert";
     QStringList arguments;
+    //コマンドを作成
     arguments << file << dir + "/" + text;
-    qDebug() << "file path:" << arguments;
     QProcess::execute(command, arguments);
     ui->label_11->setText("変換終了＆ただいま準備中…");
 }
