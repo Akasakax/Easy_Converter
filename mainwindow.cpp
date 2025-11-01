d32c58c1259f5f95c7f9022f34d7299db086bd0b
Akasaka <72452449+Akasakax@users.noreply.github.com>
2025-11-02 00:45:02 +0900

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -110,7 +110,7 @@ void MainWindow::on_pushButton_11_clicked()
 
     // 終了時の処理
     //終了したときの返される値から判断
-    connect(process, &QProcess::finished, [=](int exitCode) {
+    connect(process,static_cast<void(QProcess::*)(int, QProcess::ExitStatus)>(&QProcess::finished),this,[=](int exitCode, QProcess::ExitStatus) {
         if (exitCode == 0) {
             ui->label_11->setText("変換完了");
             ui->label_11->setStyleSheet("background-color: #339966;");
